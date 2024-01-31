e9ed34d1e01b44901ac91606171eb7c5f0f76376
tamer4k <tamer.alashraf@hotmail.com>
2024-01-31 15:19:18 +0100

    Users rights for kapsalon and barber

--- src/main/java/kapsalon/nl/repo/KapsalonRepository.java
+++ src/main/java/kapsalon/nl/repo/KapsalonRepository.java
@@ -4,6 +4,11 @@ import kapsalon.nl.models.entity.Kapsalon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface KapsalonRepository extends JpaRepository<Kapsalon, Long> {
+    Optional<Kapsalon> findByOwner(String ownerUsername);
+    List<Kapsalon> findAllByOwner(String owner);
 }
